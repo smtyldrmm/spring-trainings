fd78f8dd434e6dbb804d364b0dbc5c56536dd272
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-30 13:17:51 +0300

    DellCase

---
+++ spring-00-Coupling/src/main/java/com/cydeo/loosely_coupled/caseFactory/DellCase.java
@@ -0,0 +1,16 @@
+package com.cydeo.loosely_coupled.caseFactory;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DellCase extends Case {public DellCase(String model, String manufacturer, String powerSupply) {
+    super(model, manufacturer, powerSupply);
+}
+
+    public void pressPowerButton() {
+
+        System.out.println("Power button pressed");
+    }
+}
