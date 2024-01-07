4a6098986269487884c8b4b0e7cfe1429d4f83ad
yildirimsamet <yildirimsamet1@gmail.com>
2024-01-07 21:44:15 +0300

    DellCase

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/caseFactory/DellCase.java
@@ -0,0 +1,16 @@
+package com.cydeo.streotype_annotation.caseFactory;
+
+public class DellCase extends Case  {
+
+    public DellCase() {
+
+        super("2208","Dell","240");
+    }
+
+
+
+    public void pressPowerButton() {
+
+        System.out.println("Power button pressed");
+    }
+}
