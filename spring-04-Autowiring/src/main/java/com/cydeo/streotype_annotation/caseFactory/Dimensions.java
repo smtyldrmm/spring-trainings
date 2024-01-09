91330588ebfdf746210f42fd4711cdb218bf7ba6
yildirimsamet <yildirimsamet1@gmail.com>
2024-01-09 15:52:17 +0300

    Case-DellCase-Dimension

---
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/caseFactory/Dimensions.java
@@ -0,0 +1,26 @@
+package com.cydeo.streotype_annotation.caseFactory;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+//@AllArgsConstructor
+@Component
+public class Dimensions {
+
+    private int width;
+    private int height;
+    private int depth;
+
+    public Dimensions() {
+        this.width=10;
+        this.height=30;
+        this.depth=40;
+    }
+
+    public void pressPowerButton(){
+        System.out.println("Power button pressed");
+    }
+}
