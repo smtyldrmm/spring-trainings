bf86d0f05453d3474d4028e0a64a9728724de8c1
yildirimsamet <yildirimsamet1@gmail.com>
2024-01-10 11:25:32 +0300

    PC

---
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
@@ -0,0 +1,34 @@
+package com.cydeo.streotype_annotation;
+
+import com.cydeo.streotype_annotation.caseFactory.Case;
+import com.cydeo.streotype_annotation.monitorFactory.Monitor;
+import com.cydeo.streotype_annotation.motherboardFactory.Motherboard;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+        // Fancy graphics
+        monitor.drawPixelAt();
+    }
+
+}
